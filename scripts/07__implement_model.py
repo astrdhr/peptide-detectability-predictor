3172188fb5bf910993c0356836c2adb2b7741ec7
astrdhr <tropa1996@gmail.com>
2021-08-12 13:28:29 +0100

    Add new scripts

---
+++ scripts/07__implement_model.py
@@ -0,0 +1,28 @@
+# model architecture
+
+
+# inspect model
+
+
+# compile model
+
+
+# model training
+
+
+# predict on test set / new data
+
+
+# evaluate model on train, validation and test sets
+
+
+# evaluate using cross validation
+
+
+# get metrics
+
+
+# save results
+
+
+# save model
